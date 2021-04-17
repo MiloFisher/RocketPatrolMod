0ba21cfe63036f845385c46727d48cb469cb50d6
MiloFisher <tbfmilo@gmail.com>
2021-04-16 19:32:30 -0700

    Scoring Update

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ let spawnHeight = game.config.height - 66 - borderUISize - borderPadding;
 // SCORING
 // I went for the S(hrek) tier
 // (30) I have a simultaneous two-player mode
-// (60) I redesign the game's artwork, ui, and sounds (I made all the art, and for the sound, credit to freesound.org and I found sounds under the creative commons lisence))
+// (60) I redesign the game's artwork, ui, and sounds (I made all the art, and for the sound, credit to freesound.org and I found sounds under the creative commons license))
 // (??) I completely replaced the ship mechanic with randomized swing targets
 // (??) I set point values based on the randomized speeds of said targets
 // (??) I created an animation for showing the point values when hitting a target
